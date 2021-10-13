f92b97c06b40ed05b33efd1f6fefc4db0620daf0
TapanManu <tapanmanu2000@gmail.com>
2021-10-13 23:22:46 +0530

    responsiveness added further

--- js/nav.js
+++ js/nav.js
@@ -1,8 +1,9 @@
 var menuButton = document.getElementById("hamburger");
 var menu_left = document.querySelector(".nav-left-menu");
 var menu_right = document.querySelector(".nav-right-menu");
+// menu_left.innerHTML += menu_right.innerHTML;
 menuButton.addEventListener("click", () => {
+    
     console.log(menu_left.classList);
     menu_left.classList.toggle("show-menu");
-    
 });        
