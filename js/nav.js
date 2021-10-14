b9cdb1b721b06e79583b6431ac451bacedead23d
TapanManu <tapanmanu2000@gmail.com>
2021-10-14 12:27:58 +0530

    responsiveness to nav

--- js/nav.js
+++ js/nav.js
@@ -1,9 +1,17 @@
 var menuButton = document.getElementById("hamburger");
 var menu_left = document.querySelector(".nav-left-menu");
 var menu_right = document.querySelector(".nav-right-menu");
+var menu = menu_left.cloneNode(true);
+click = false;
 // menu_left.innerHTML += menu_right.innerHTML;
 menuButton.addEventListener("click", () => {
-    
+    click = !click;
     console.log(menu_left.classList);
+    if(click){
+        menu_left.innerHTML += menu_right.innerHTML;
+    }
+    else{
+        menu_left.innerHTML = menu.innerHTML;
+    }
     menu_left.classList.toggle("show-menu");
 });        
